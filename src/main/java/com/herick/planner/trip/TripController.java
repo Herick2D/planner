ef2be1f6325be0d475ef58244b5b66ea024697f1
Herick Moreira <123032507+Herick2D@users.noreply.github.com>
2024-07-09 19:13:04 -0300

    feat: método put para atualizações de viagens já criadas

--- src/main/java/com/herick/planner/trip/TripController.java
+++ src/main/java/com/herick/planner/trip/TripController.java
@@ -6,10 +6,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -37,6 +40,25 @@ public class TripController {
   @GetMapping("/{tripId}")
   public ResponseEntity<Trip> getTripDetails(@PathVariable UUID tripId) {
     Optional<Trip> trip = this.repository.findById(tripId);
+
+    return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+
+  }
+
+  @PutMapping("/{tripId}")
+  public ResponseEntity<Trip> updateTrip(@PathVariable UUID tripId, @RequestBody TripRequestPayload payload) {
+    Optional<Trip> trip = this.repository.findById(tripId);
+
+    if(trip.isPresent()){
+      Trip rawTrip = trip.get();
+      rawTrip.setEndsAt(LocalDateTime.parse(payload.ends_at(), DateTimeFormatter.ISO_DATE_TIME));
+      rawTrip.setStartsAt(LocalDateTime.parse(payload.starts_at(), DateTimeFormatter.ISO_DATE_TIME));
+      rawTrip.setDestination(payload.destination());
+
+      this.repository.save(rawTrip);
+
+      return ResponseEntity.ok(rawTrip);
+    }
 
     return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
 
