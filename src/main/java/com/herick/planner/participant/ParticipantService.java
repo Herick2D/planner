c9f68e7c113bbf6212ac690fc853aaa5f1ba5be9
Herick Moreira <123032507+Herick2D@users.noreply.github.com>
2024-07-10 17:43:06 -0300

    feat: método para seja possível convidar participantes para viagem, além deles poderem confirmar

--- src/main/java/com/herick/planner/participant/ParticipantService.java
+++ src/main/java/com/herick/planner/participant/ParticipantService.java
@@ -22,5 +22,15 @@ public class ParticipantService {
 
   }
 
+  public ParticipantCreateResponse registerParticipantToEvent(String email, Trip trip){
+    Participant participant = new Participant(email, trip);
+
+    this.repository.save(participant);
+
+    return new ParticipantCreateResponse(participant.getId());
+  }
+
   public void triggerConfirmationEmailToParticipants(UUID tripId){}
+
+  public void triggerConfirmationEmailToParticipant(String email){}
 }
--- src/main/java/com/herick/planner/trip/TripController.java
+++ src/main/java/com/herick/planner/trip/TripController.java
@@ -1,5 +1,7 @@
 package com.herick.planner.trip;
 
+import com.herick.planner.participant.ParticipantCreateResponse;
+import com.herick.planner.participant.ParticipantRequestPayload;
 import com.herick.planner.participant.ParticipantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -78,6 +82,22 @@ public class TripController {
     }
 
     return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+  }
+
+  @PostMapping("/{tripId}/invite")
+  public ResponseEntity<ParticipantCreateResponse> something(@PathVariable UUID tripId, @RequestBody ParticipantRequestPayload payload) {
+    Optional<Trip> trip = this.repository.findById(tripId);
+
+    if(trip.isPresent()){
+      Trip rawTrip = trip.get();
+
+      ParticipantCreateResponse participantResponse =  this.participantService.registerParticipantToEvent(payload.email(), rawTrip);
+
+      if(rawTrip.getIsConfirmed()) this.participantService.triggerConfirmationEmailToParticipant(payload.email());
+
+      return ResponseEntity.ok(participantResponse);
+    }
+    return ResponseEntity.notFound().build();
   }
 
 }
